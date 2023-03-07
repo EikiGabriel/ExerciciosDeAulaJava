177c20e7a9913d49d211598de1d33ca14afe7f94
EikiGabriel <113028393+EikiGabriel@users.noreply.github.com>
2023-03-06 21:40:23 -0300

    exercicios

---
+++ exer.java
@@ -0,0 +1,239 @@
+package exercicios;
+import java.util.Scanner;
+
+
+public class exer {
+/*
+   public static void main(String[] args) {
+
+       Scanner leia = new Scanner(System.in);
+
+       System.out.println("Digite um numero: ");
+
+       int num1 = leia.nextInt();
+
+     
+       System.out.print("A soma dos numeros é :");
+       System.out.print(num1 * 2);       
+       
+       
+   }
+   */
+	/*
+	public static void main(String[] args) {
+		
+		Scanner leia = new Scanner(System.in);
+		
+		System.out.println("Digite um numero: ");
+
+	    int num1 = leia.nextInt();
+
+	    System.out.println("Digite outro numero");
+
+	    int num2 = leia.nextInt();
+
+	    System.out.print("A media aritimetica é :");
+	    System.out.print((num1 + num2)/2);    
+	    
+	    
+	    System.out.println("");
+	    
+	    
+	    System.out.println("A metade da diferença é: ");
+	    System.out.print((num1 - num2)/2); 
+	}
+	*/
+	/*
+	public static void main(String[] args) {
+		
+		Scanner leia = new Scanner(System.in);
+		
+		System.out.println("Qual foi a sua nota da primeira prova? ");
+
+	    int nota1 = leia.nextInt();
+
+	    System.out.println("Qual foi a sua nota da segunda prova? ");
+
+	    int nota2 = leia.nextInt();
+	    
+	    System.out.println("Qual foi a sua nota da terceira prova? ");
+
+	    int nota3 = leia.nextInt();
+
+
+	    System.out.print("A sua media é :");
+	    System.out.print((nota1 + nota2 + nota3)/3);    
+	}
+	*/
+	/*
+	public static void main(String[] args) {
+		
+		Scanner leia = new Scanner(System.in);
+		
+		System.out.println("Digite seu salario...");
+
+	    float salario = leia.nextInt();
+
+	    System.out.print("O seu salario é:");
+	    System.out.print(salario);  
+	    
+	    
+	    System.out.print("O seu desconto é:");
+	    System.out.print((salario * 1.08) - salario);
+	    
+	    
+	    
+	    System.out.print("O salario liquido é:");
+	    System.out.print(salario * 0.92);    
+ 
+	}
+	
+	*/
+	/*
+	public static void main(String[] args) {
+		
+		Scanner leia = new Scanner(System.in);
+		
+		System.out.println("O valor de A");
+	    int A = leia.nextInt();
+
+	    System.out.println("O valor de B");
+	    int B = leia.nextInt();
+
+	    System.out.println("O valor de C");
+	    int C = leia.nextInt();
+	    
+	    int D = (B*B) - 4*A*C;
+	    
+	    System.out.print("O calculo deu:");
+	    System.out.print(D);    
+
+	}
+	*/
+	/*
+	public static void main(String[] args) {
+		
+		Scanner leia = new Scanner(System.in);
+		
+		System.out.println("Quantos Km");
+	    int KM = leia.nextInt();
+
+	   int metros = KM *1000;
+	    
+
+	    
+	    System.out.print("A distancia em metros é:");
+	    System.out.print(metros);    
+
+	}
+
+	*/
+	/*
+	public static void main(String[] args) {
+		
+		Scanner leia = new Scanner(System.in);
+		
+		System.out.println("Qual a quilometragem atual: ");
+	    int KM_ini = leia.nextInt();
+
+	    System.out.println("E a quilometragem final: ");
+	    int KM_final = leia.nextInt();
+
+
+	    System.out.print("A kilometragem percorrida foi: ");
+	    System.out.print(KM_final - KM_ini);    
+
+	}
+	
+	*/
+	/*
+	public static void main(String[] args) {
+		
+		Scanner leia = new Scanner(System.in);
+		
+		System.out.println("O valor de A");
+	    int A = leia.nextInt();
+
+	    System.out.println("O valor de B");
+	    int B = leia.nextInt();
+
+	    System.out.println("O valor de C");
+	    int C = leia.nextInt();
+	    
+	    int D = (B*B) - 4*A*C;
+	    
+	    System.out.print("O calculo deu:");
+	    System.out.print(D);    
+
+	}
+	*/
+	/*
+	public static void main(String[] args) {
+		
+		Scanner leia = new Scanner(System.in);
+		
+		System.out.println("O valor de A");
+	    int A = leia.nextInt();
+
+	    System.out.println("O valor de B");
+	    int B = leia.nextInt();
+
+	    System.out.println("O valor de C");
+	    int C = leia.nextInt();
+	    
+	    int D = (B*B) - 4*A*C;
+	    
+	    System.out.print("O calculo deu:");
+	    System.out.print(D);    
+
+	}
+	*/
+	/* 88888888888888888888888
+	public static void main(String[] args) {
+		
+		Scanner leia = new Scanner(System.in);
+		
+		System.out.println("O valor de A");
+	    int A = leia.nextInt();
+
+	    System.out.println("O valor de B");
+	    int B = leia.nextInt();
+
+	    System.out.println("O valor de C");
+	    int C = leia.nextInt();
+	    
+	    int D = (B*B) - 4*A*C;
+	    
+	    System.out.print("O calculo deu:");
+	    System.out.print(D);    
+	*/
+	
+	*/
+	public static void main(String[] args) {
+		
+		Scanner leia = new Scanner(System.in);
+		
+		System.out.println("Qual a quantidade de mini-game vendidas hoje: ");
+	    int minigame = leia.nextInt();
+
+	    System.out.println("Qual a quantidade de caculadoras vendidas hoje: ");
+	    int calculadora = leia.nextInt();
+
+	    System.out.println("Qual a quantidade de canetas vendidas hoje: ");
+	    int canetas = leia.nextInt();
+	    
+	    float minical = (float) (minigame * 6.95);
+	    float calcal = (float) (calculadora * 3.50);
+	    float cancal = minigame * (float) 1.20;
+	    
+
+	    System.out.print("A fatura de hoje deu: R$");
+	    System.out.print(minical + calcal + cancal);
+	
+	
+	}
+	*/
+	
+}
+
+   
